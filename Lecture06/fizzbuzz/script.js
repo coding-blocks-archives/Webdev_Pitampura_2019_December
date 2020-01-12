703514d33175bac25c0f3e7f88e602c47e871ff4
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2020-01-12 19:09:21 +0530

    fizzbuzz optimsed approach
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- Lecture06/fizzbuzz/script.js
+++ Lecture06/fizzbuzz/script.js
@@ -3,18 +3,43 @@ window.onload = function () {
     let btn = document.getElementById('btn')
     let number = document.getElementById('number')
 
+    // btn.onclick = function () {
+    //     let start = new Date().getTime()
+    //     let n = parseInt(number.value)
+    //     let listContent = ''
+
+    //     for (let i = 1; i <= n; i++) {
+    //         let print = ''
+    //         if (i % 3 == 0) print += 'fizz'
+    //         if (i % 5 == 0) print += 'buzz'
+    //         if (print == '') print += i
+
+    //         listContent += `<li>${print}</li>`
+    //     }
+    //     list.innerHTML = listContent
+    //     let end = new Date().getTime()
+
+    //     console.log(end - start)
+    // }
+
     btn.onclick = function () {
+        let start = new Date().getTime()
         let n = parseInt(number.value)
-        let listContent = ''
 
         for (let i = 1; i <= n; i++) {
             let print = ''
             if (i % 3 == 0) print += 'fizz'
             if (i % 5 == 0) print += 'buzz'
-            if (print == '') print += i
+            if (print == '') print = i
 
-            listContent += `<li>${print}</li>`
+            let item = document.createElement('li')
+            item.innerText = print
+
+            list.appendChild(item)
         }
-        list.innerHTML = listContent
+
+        let end = new Date().getTime()
+
+        console.log(end - start)
     }
 }
