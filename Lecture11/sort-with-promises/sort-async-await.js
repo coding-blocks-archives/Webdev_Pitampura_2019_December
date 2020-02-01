c701fbc6019bb4f8bbf594276a4d465b2bfc0c77
Arnav Gupta <arnav@codingblocks.com>
2020-02-01 19:19:21 +0530

    sort with async await
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture11/sort-with-promises/sort-async-await.js
@@ -0,0 +1,38 @@
+const fs = require('fs').promises
+const os = require('os')
+
+async function task() {
+  try {
+    console.log('read started')
+    let p1 = fs.readFile('./input1.txt')
+    let p2 = fs.readFile('./input2.txt')
+    let p3 = fs.readFile('./input3.txt')
+
+    let data1 = await p1
+    console.log('data 1 read')
+    let data2 = await p2
+    console.log('data 2 read')
+    let data3 = await p3
+    console.log('data 3 read')
+
+    let sorted = data1
+      .toString()
+      .split(os.EOL)
+      .concat(data2.toString().split(os.EOL))
+      .concat(data3.toString().split(os.EOL))
+      .sort((a, b) => a - b)
+      .join(os.EOL)
+
+    await fs.writeFile('./output2.txt', sorted)
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+task().then(() => {
+  console.log('task really done')
+})
+
+setInterval(() => {
+  console.log('tick')
+}, 1)
--- Lecture11/sort-with-promises/sort-promise.js
+++ Lecture11/sort-with-promises/sort-promise.js
@@ -1,9 +1,24 @@
 const fs = require('fs').promises
+const os = require('os')
 
 fs.readFile('./input1.txt')
-  .catch((err) => {
-    console.error(err)
-  })
-  .then((data) => {
-    console.log(data.toString())
+  .then((data1) => {
+    fs.readFile('./input2.txt')
+      .then((data2) => {
+        fs.readFile('./input3.txt')
+          .then((data3) => {
+
+            let sorted = data1.toString().split(os.EOL)
+              .concat(data2.toString().split(os.EOL))
+              .concat(data3.toString().split(os.EOL))
+              .sort((a,b) => a - b)
+              .join(os.EOL)
+
+            fs.writeFile('./output.txt', sorted)
+              .then(() => {
+                console.log('sorting done')
+              })
+
+          })
+      })
   })
