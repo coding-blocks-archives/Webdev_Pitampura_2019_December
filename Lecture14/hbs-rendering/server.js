a04ab9182fe5cb64f0cd8a073146e54ce501cde0
Arnav Gupta <arnav@codingblocks.com>
2020-02-16 17:41:14 +0530

    handle post requests
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture14/hbs-rendering/server.js
+++ Lecture14/hbs-rendering/server.js
@@ -10,11 +10,28 @@ let todos = [
     {name: 'one more task', done: false},
 ]
 
+// These are middlewares that help parse the req.body
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
 app.get('/',(req, res) => {
     res.render('todo', {
         title: 'Todo List',
         todos: todos
     })
+})
+
+
+app.post('/add', (req, res) => {
+    console.log('POST /add')
+    console.log(req.body)
+    if (req.body.newtask) {
+        todos.push({
+            name: req.body.newtask,
+            done: false
+        })
+    }
+    res.redirect('/')
 })
 
 app.listen(5432, () => {
