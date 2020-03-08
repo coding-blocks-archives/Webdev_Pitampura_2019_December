5815de196d31a45139bd5b2693b6d59efd3cd33a
Arnav Gupta <arnav@codingblocks.com>
2020-03-08 17:18:51 +0530

    comment the variables
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture19/websockets/server.js
+++ Lecture19/websockets/server.js
@@ -1,10 +1,15 @@
+// the express library
 const express = require('express')
+// the http library (comes with nodejs by default)
 const http = require('http')
+// the socket.io library
 const socketio = require('socket.io')
 
-
+// the express app
 const app = express()
+// the http server created with logic of the app
 const server = http.createServer(app)
+// the socket.io listener created on the server 
 const io = socketio(server)
 
 app.use('/', express.static(__dirname + '/public'))
